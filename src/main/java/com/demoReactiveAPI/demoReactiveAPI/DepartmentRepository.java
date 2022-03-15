aca401eaefd444fb343d24a966b639161947183b
Omair Iqbal <omair95@protonmail.com>
2022-03-15 20:13:16 +0000

    First commit

---
+++ src/main/java/com/demoReactiveAPI/demoReactiveAPI/DepartmentRepository.java
@@ -0,0 +1,8 @@
+package com.demoReactiveAPI.demoReactiveAPI;
+
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import reactor.core.publisher.Mono;
+
+public interface DepartmentRepository extends ReactiveCrudRepository<Department,Integer> {
+    Mono<Department> findByUserId(Integer userId);
+}
---
+++ src/test/java/com/demoReactiveAPI/demoReactiveAPI/DemoReactiveApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demoReactiveAPI.demoReactiveAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoReactiveApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
